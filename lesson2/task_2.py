517d63e381889e8e13a71186a44ed44a00bc0597
OlgaLitv <m-savenok@mail.ru>
2021-03-01 15:22:39 +0300

    Задача 2, исправила обработку случаев, когда слово начинается со знака + или -

--- lesson2/task_2.py
+++ lesson2/task_2.py
@@ -12,14 +12,16 @@
 """
 
 weather_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-index = 0
 weather_list2 = []
 weather_string = ""
 for element in weather_list:
     is_integer = True
-    for letter in element:
-        if not (ord('0') <= ord(letter) <= ord('9') or element[0] == '-' or element[0] == '+'):
-            is_integer = False
+    if element[0] == '-' or element[0] == '+' or ord('0') <= ord(element[0]) <= ord('9'):
+        for letter in element[1:]:
+            if not (ord('0') <= ord(letter) <= ord('9')):
+                is_integer = False
+    else:
+        is_integer = False
     if is_integer and element != '':
         weather_list2.append('"')
         if element[0] == '-' or element[0] == '+':
@@ -29,10 +31,8 @@ for element in weather_list:
             weather_list2.append(f'{int(element):02d}')
             weather_string += '"' + f'{int(element):02d}' + '" '
         weather_list2.append('"')
-        index += 2
     else:
         weather_list2.append(element)
         weather_string += element + ' '
-    index += 1
 print('новый список:', weather_list2)
 print('строка:', weather_string)
