d0e789132dcdaa582973f944cdfa21d303ecac5c
OlgaLitv <m-savenok@mail.ru>
2021-03-28 15:05:55 +0300

    задача 3 урока 8: добавила wraps и вывод типа возвращаемого значения

--- lesson8/task3.py
+++ lesson8/task3.py
@@ -14,9 +14,11 @@ def calc_cube(x):
 #>>> a = calc_cube(5)
 calc_cube(5: <class 'int'>)"""
 
+from functools import wraps
+
 
 def type_logger(func):
-
+    @wraps(func)
     def tag_logger(*args, **kwargs):
         answer = str(func.__name__) + '('
         for arg in args:
@@ -25,21 +27,31 @@ def type_logger(func):
             answer += str(kw) + ': ' + str(type(kw)) + ', '
         answer = answer[:-2] + ')'
         print('---type_logger:', answer)
-        return func(*args, **kwargs)
+        res = func(*args, **kwargs)
+        # tag_logger.__doc__ = func.__doc__
+        print('type of return value:', type(res))
+        return res
 
     return tag_logger
 
 
 @type_logger
 def calc_cube(x):
+    """calc_cube -  вычисляет куб числа"""
     return x ** 3
 
 
 @type_logger
 def calc_degree(number=1, degree=1):
+    """ вычисляет число number  в степени degree """
     return number ** degree
 
 
 print('куб числа 3 равен', calc_cube(3))
+print('описание:', calc_cube.__doc__)
+print('------------------------------------------------')
 print('4 в степени 2 равно ', calc_degree(degree=2, number=4))
+print('описание:', calc_degree.__doc__)
+print('------------------------------------------------')
 print('5 в степени 3 равно ', calc_degree(5, degree=3))
+print('описание:', calc_degree.__doc__)
